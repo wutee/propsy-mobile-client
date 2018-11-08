2777153fb3ba290681fcec2cc6a3c98ff1999ecd
trzaskom <m_trzaskowski@wp.pl>
2018-11-08 19:46:55 +0100

    Remove redundant comment

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -13,24 +13,3 @@ export class HomePage {
     this.router.navigateByUrl('/restaurant');
   }
 }
-
-
-
-/*
-import { Component } from '@angular/core';
-import { NavController } from '@ionic/angular';
-import { RestaurantPage } from '../restaurant/restaurant.page';
-
-
-@Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss']
-})
-export class HomePage {
-  constructor(private navCtrl: NavController) {}
-
-  listRestaurants() {
-    this.navCtrl.push(RestaurantPage);
-  }
-}*/
