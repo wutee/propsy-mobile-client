eae34fc1a19840441addd9e6d7ebfc2f2fd78049
scresh <screshowski@gmail.com>
2018-11-09 11:01:25 +0100

    Add order listing

--- src/app/orders/orders.page.ts
+++ src/app/orders/orders.page.ts
@@ -8,8 +8,29 @@ import 'hammerjs';
 })
 export class OrdersPage implements OnInit {
   constructor() {
-
   }
+
+  items = [
+    'Pokémon Yellow',
+    'Super Metroid',
+    'Mega Man X',
+    'The Legend of Zelda',
+    'Pac-Man',
+    'Super Mario World',
+    'Street Fighter II',
+    'Half Life',
+    'Final Fantasy VII',
+    'Star Fox',
+    'Tetris',
+    'Donkey Kong III',
+    'GoldenEye 007',
+    'Doom',
+    'Fallout',
+    'GTA',
+    'Halo'
+  ];
+
+
   pressEvent() {
     console.log('press');
   }
